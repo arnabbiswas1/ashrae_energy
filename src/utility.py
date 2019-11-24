a8cbf7fdbf4f4ce22cc3871e024b8f1e30752e14
Arnab <arnabbiswas1@outlook.com>
2019-11-24 09:27:05 +0000

    feature engineering

--- src/utility.py
+++ src/utility.py
@@ -426,3 +426,27 @@ def plot_meter_reading_for_site(df, site_id):
         fig.show()        
 
 
+
+def concat_features(source_df, target_df, f1, f2):
+    target_df[f'{f1}_{f2}'] =  source_df[f1].astype(str) + '_' + source_df[f2].astype(str)
+    return target_df
+
+
+def create_interaction_features(source_df, target_df):
+    target_df = concat_features(source_df, target_df, 'site_id', 'building_id')
+    target_df['site_building_meter_id'] = source_df.site_id.astype(str) + '_' + source_df.building_id.astype(str) + '_' + source_df.meter.astype(str)
+    target_df['site_building_meter_id_usage'] = source_df.site_id.astype(str) + '_' + source_df.building_id.astype(str) + '_' + source_df.meter.astype(str) + '_' + source_df.primary_use
+
+    target_df = concat_features(source_df, target_df, 'site_id', 'meter')
+    target_df = concat_features(source_df, target_df, 'building_id', 'meter')
+    
+    target_df = concat_features(source_df, target_df, 'site_id', 'primary_use')
+    target_df = concat_features(source_df, target_df, 'building_id', 'primary_use')
+    target_df = concat_features(source_df, target_df, 'meter', 'primary_use')
+    
+    return target_df
+
+
+def create_age(source_df, target_df, f):
+    target_df['building_age'] = 2019 - source_df[f]
+    return target_df
